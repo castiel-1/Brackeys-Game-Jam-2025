80c75fec2226679f1c11112d08a2a0a8a4803815
castiel-1 <qwrtvbfeuerherz@gmail.com>
2025-08-19 23:09:34 +0200

    basic stuff
    - set up folder structure
    - programmed volume mixer for master, music, soundfx
    - set up UI
    - set up hierarchy

---
+++ Assets/Scripts/Audio/SoundFXManager.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioSource soundFXPrefab;
+
+    private void Awake()
+    {
+        if(Instance !=  null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void PlaySoundFXClip(AudioClip clip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(soundFXPrefab, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayRandomSoundFXClip(AudioSource[] clips, Transform spawnTransform, float volume)
+    {
+        int rand = Random.Range(0, clips.Length);
+
+        AudioSource audioSource = clips[rand];
+
+        audioSource.clip = audioSource.clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+
+    }
+}
