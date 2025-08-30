c4684e0868a2a60e5dea0b2f18ee755d3f6c2859
castiel-1 <qwrtvbfeuerherz@gmail.com>
2025-08-30 21:56:45 +0200

    doors are working!

--- Assets/Scripts/Objects/Item/LootBox.cs
+++ Assets/Scripts/Objects/Item/LootBox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class LootBox : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject _lootPrefab;
-    [SerializeField] private Vector2 _lootSpawnDirection;
+    [SerializeField] private GameObject _lootSpawnLocation;
     [SerializeField] private Sprite _emptyLootBox;
 
     [SerializeField] private AudioClip _lootSpawnSound;
@@ -24,7 +24,7 @@ public class LootBox : MonoBehaviour, IInteractable
         SoundFXManager.Instance.PlaySoundFXClip(_lootSpawnSound, transform, _volume);
 
         GameObject loot = Instantiate(_lootPrefab, transform);
-        loot.transform.position = (Vector2)transform.position + _lootSpawnDirection;
+        loot.transform.position = transform.position + (_lootSpawnLocation.transform.position - transform.position);
 
         GetComponent<SpriteRenderer>().sprite = _emptyLootBox;
 
